21f8e82cc450f05fbcc2c66e3f69c660c32f811d
renewal398 <renewal398@gmail.com>
2025-09-17 16:39:36 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'gradient-shift': 'gradient-shift 4s ease infinite',
+      },
+      keyframes: {
+        'gradient-shift': {
+          '0%, 100%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
