853916b3b6829ac485b710e5bec6aa30cba1bd54
renewal398 <renewal398@gmail.com>
2025-09-17 16:23:22 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReddIQ from './components/ReddIQ';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ReddIQ />
+    </div>
+  );
+}
+
+export default App;
